14985362c6a9261facc8db091af6783814f4cb63
MarTep <34692617+MarTep@users.noreply.github.com>
2022-05-26 11:58:27 +0300

    Add files via upload

---
+++ src/main/java/gr/upatras/rest/example/Application.java
@@ -0,0 +1,15 @@
+package gr.upatras.rest.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+// Added new lines for new push to develop
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
